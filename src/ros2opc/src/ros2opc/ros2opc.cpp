ae90c6c0419eeb3eba21b03ff6100e6f266fbed7
flixz02 <66864107+flixz02@users.noreply.github.com>
2022-09-12 14:30:09 +0200

    first "Hello World" version of open62541 for ros humble

---
+++ src/ros2opc/src/ros2opc/ros2opc.cpp
@@ -0,0 +1,69 @@
+#include <memory>
+#include <open62541.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+
+// ROS NODE
+
+class MinimalSubscriber : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const std_msgs::msg::String & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
+
+// OPC Client
+
+// int opcClient(void) {
+//     UA_Client *client = UA_Client_new();
+//     UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+//     UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://localhost:4840");
+//     if(retval != UA_STATUSCODE_GOOD) {
+//         UA_Client_delete(client);
+//         return (int)retval;
+//     }
+// 
+//     /* Read the value attribute of the node. UA_Client_readValueAttribute is a
+//      * wrapper for the raw read service available as UA_Client_Service_read. */
+//     UA_Variant value; /* Variants can hold scalar values and arrays of any type */
+//     UA_Variant_init(&value);
+// 
+//     /* NodeId of the variable holding the current time */
+//     const UA_NodeId nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME);
+//     retval = UA_Client_readValueAttribute(client, nodeId, &value);
+// 
+//     if(retval == UA_STATUSCODE_GOOD &&
+//        UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME])) {
+//         UA_DateTime raw_date = *(UA_DateTime *) value.data;
+//         UA_DateTimeStruct dts = UA_DateTime_toStruct(raw_date);
+//         UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "date is: %u-%u-%u %u:%u:%u.%03u\n",
+//                     dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
+//     }
+// 
+//     /* Clean up */
+//     UA_Variant_clear(&value);
+//     UA_Client_delete(client); /* Disconnects the client internally */
+//     return EXIT_SUCCESS;
+// }
